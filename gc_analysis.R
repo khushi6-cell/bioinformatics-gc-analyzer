a7d8acb54274b68bda6a65dcae119e991160c09c
khushi6-cell <khushiaggarwal255@gmail.com>
2026-02-26 17:55:27 +0530

    Update gc_analysis.R
    Added dynamic input and AT content calculation

--- gc_analysis.R
+++ gc_analysis.R
@@ -4,7 +4,13 @@
 library(stringr)
 
 # Read FASTA file
-lines <- readLines(file.path(getwd(), "sample_sequences.fasta"))
+file_name <- readline(prompt="Enter FASTA file name: ")
+
+if (!file.exists(file_name)) {
+  stop("File does not exist. Please check the file name.")
+}
+
+lines <- readLines(file_name)
 
 # Extract headers (lines starting with >)
 headers <- lines[grepl("^>", lines)]
@@ -20,21 +26,31 @@ calculate_gc <- function(seq) {
   gc_content <- (g_count + c_count) / total_length
   return(gc_content)
 }
-
+calculate_at <- function(seq) {
+  a_count <- str_count(seq, "A")
+  t_count <- str_count(seq, "T")
+  total_length <- nchar(seq)
+  at_content <- (a_count + t_count) / total_length
+  return(at_content)
+}
 # Apply function to all sequences
 gc_values <- sapply(sequences, calculate_gc)
+at_values <- sapply(sequences, calculate_at)
 
 # Create results data frame
 results <- data.frame(
   Sequence = sub("^>", "", headers),
   Length = nchar(sequences),
-  GC_Content = gc_values
+  GC_Content = gc_values,
+  AT_Content = at_values
 )
-
-print(results)
 
 # Plot GC content distribution
 hist(results$GC_Content,
      main = "GC Content Distribution",
      xlab = "GC Content",
      col = "lightblue")
+hist(results$AT_Content,
+     main = "AT Content Distribution",
+     xlab = "AT Content",
+     col = "pink")
